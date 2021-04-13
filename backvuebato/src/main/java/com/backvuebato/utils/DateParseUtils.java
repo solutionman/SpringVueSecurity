919439a6e5b01266ee876e25f0694e3f8639ede1
solutionman <solutionman@inbox.ru>
2021-04-13 12:49:44 +0700

    date formatting

--- backvuebato/src/main/java/com/backvuebato/utils/DateParseUtils.java
+++ backvuebato/src/main/java/com/backvuebato/utils/DateParseUtils.java
@@ -1,12 +1,20 @@
 package com.backvuebato.utils;
 
 import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class DateParseUtils {
     public Date stringToSqlDate(Object date){
         try {
             String dateToString = date.toString();
-            Date result = java.sql.Date.valueOf(dateToString);
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy", new Locale("ru"));
+            LocalDate localDate = LocalDate.parse(dateToString, formatter);
+            Date result = java.sql.Date.valueOf(localDate);
+
             return result;
         } catch (Exception e){
             return null;
