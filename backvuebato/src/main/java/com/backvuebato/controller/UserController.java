55d720720f3a74cf7de603f4484b409fd2894256
solutionman <solutionman@inbox.ru>
2021-05-10 12:16:07 +0700

    move delete to userController

--- backvuebato/src/main/java/com/backvuebato/controller/UserController.java
+++ backvuebato/src/main/java/com/backvuebato/controller/UserController.java
@@ -10,6 +10,10 @@ import com.backvuebato.utils.DateParseUtils;
 import com.fasterxml.jackson.databind.util.JSONPObject;
 import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -126,4 +130,66 @@ public class UserController {
         rolesMap.put("roles", roles);
         return rolesMap;
     }
+
+    @PostMapping(value = "delete")
+    public Map<String, Object> deletePersons(@RequestBody Map<String, Object> data) {
+        // TODO delete selected
+        String selected = data.get("selected").toString();
+        String sortDesc = data.get("sortDesc").toString();
+        int itemsPerPage = Integer.parseInt(data.get("itemsPerPage").toString());
+        String orderBy = data.get("sortBy").toString();
+        String forPageable;
+        switch (orderBy) {
+            case "[familyname]":
+                forPageable = "familyname";
+                break;
+            case "[firstname]":
+                forPageable = "firstname";
+                break;
+            case "[middlename]":
+                forPageable = "middlename";
+                break;
+            case "[birthday]":
+                forPageable = "birthday";
+                break;
+            default:
+                forPageable = "id";
+        }
+        int currPage = Integer.parseInt(data.get("page").toString()) - 1;
+        Pageable pageable = PageRequest.of(currPage, itemsPerPage, Sort.by(sortDesc.equals("[true]") ? Sort.Direction.DESC : Sort.Direction.ASC, forPageable));
+        Page<Persons> page;
+        String search = null == data.get("search") ? "" : data.get("search").toString();
+//        Long personID = Long.parseLong( data.get("id").toString(), 10 );
+        if (search.isEmpty()) {
+            page = personRepository.findAll(pageable);
+        } else {
+            page = personRepository.findByFamilynameContainingIgnoreCaseOrFirstnameContainingIgnoreCaseOrMiddlenameContainingIgnoreCase(
+                    search, search, search, pageable);
+        }
+
+        List<Persons> personsList = page.getContent();
+
+        List<Map<String, Object>> personsToDisplay = new ArrayList<>();
+        for (Persons person : personsList) {
+            Map<String, Object> currentPerson = new HashMap<>();
+
+            currentPerson.put("id", person.getId());
+            Users user = userRepository.findById(person.getUserid());
+            currentPerson.put("name", user.getUsername());
+            currentPerson.put("familyname", person.getFamilyname());
+            currentPerson.put("firstname", person.getFirstname());
+            currentPerson.put("middlename", person.getMiddlename());
+            currentPerson.put("birthday", dateParseUtils.sqlDateToString(person.getBirthday()));
+            currentPerson.put("additionalData", "some data to show about this person, for example email: "
+                    + (person.getEmail() == null ? "" : person.getEmail()));
+            personsToDisplay.add(currentPerson);
+        }
+        long totalPersons = page.getTotalElements();
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("persons", personsToDisplay);
+        resultMap.put("totalPersons", totalPersons);
+
+        return resultMap;
+    }
+
 }
