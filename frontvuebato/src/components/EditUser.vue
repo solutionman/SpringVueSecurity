9f24af1be8957a1051bf820303b71128ebb94160
solutionman <solutionman@inbox.ru>
2021-05-14 12:12:35 +0700

    edit user change pass

--- frontvuebato/src/components/EditUser.vue
+++ frontvuebato/src/components/EditUser.vue
@@ -8,7 +8,7 @@
                 pa-2
             >
                 <v-card-title>
-                    Edit user.
+                    Edit user
                     <v-spacer></v-spacer>
                 </v-card-title>
 
@@ -102,6 +102,67 @@
                     </form>
                 </ValidationObserver>
 
+                <v-spacer></v-spacer>
+                <v-spacer></v-spacer>
+
+                <v-dialog
+                    v-model="dialog"
+                    persistent
+                    max-width="600px"
+                >
+                    <template v-slot:activator="{ on, attrs }">
+                        <v-btn
+                            color="primary"
+                            dark
+                            v-bind="attrs"
+                            v-on="on"
+                        >
+                            Change Pass
+                        </v-btn>
+                    </template>
+                    <v-card>
+                        <v-card-title>
+                            <span class="headline">User Profile</span>
+                        </v-card-title>
+                        <v-card-text>
+                            <v-container>
+                                <v-row>
+                                    <v-col cols="12">
+                                        <v-text-field
+                                            label="Password*"
+                                            required
+                                        ></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12">
+                                        <v-text-field
+                                            label="Confirm Password*"
+                                            required
+                                        ></v-text-field>
+                                    </v-col>
+                                </v-row>
+                            </v-container>
+                            <small>*indicates required field</small>
+                        </v-card-text>
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn
+                                color="blue darken-1"
+                                text
+                                @click="dialog = false"
+                            >
+                                Close
+                            </v-btn>
+                            <v-btn
+                                color="blue darken-1"
+                                text
+                                @click="dialog = false"
+                            >
+                                Save
+                            </v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-dialog>
+
             </v-flex>
 
             <v-flex
@@ -161,6 +222,7 @@ export default {
             select: null,
             roles: [],
             selectedRoles: [],
+            dialog: false
         }
     },
     mounted() {
