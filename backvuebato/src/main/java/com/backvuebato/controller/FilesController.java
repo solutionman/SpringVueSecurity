d5d8c259ef8725bc8c7ebbd8013cade4c7ccb1ca
solutionman <solutionman@inbox.ru>
2021-07-14 12:25:49 +0700

    files

---
+++ backvuebato/src/main/java/com/backvuebato/controller/FilesController.java
@@ -0,0 +1,26 @@
+package com.backvuebato.controller;
+
+import com.backvuebato.repository.FilesRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class FilesController {
+    private final FilesRepository filesRepository;
+
+    FilesController(FilesRepository filesRepository) {
+        this.filesRepository = filesRepository;
+    }
+
+    @PostMapping("storeFile")
+    public Map<String, Object> storeFile(@RequestParam("file") MultipartFile file) {
+        String debug = "";
+
+        return new HashMap<>();
+    }
+}
