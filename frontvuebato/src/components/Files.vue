3f21f74f1e30660e8fbcb1995189802f5f5cf21e
solutionman <solutionman@inbox.ru>
2021-07-15 08:48:23 +0700

    files

--- frontvuebato/src/components/Files.vue
+++ frontvuebato/src/components/Files.vue
@@ -1,11 +1,38 @@
 <template>
     <v-card>
-        <v-file-input
-            show-size
-            label="File input"
-        ></v-file-input>
+        <form>
+            <v-file-input
+                v-model="file"
+                show-size
+                label="File input"
+            ></v-file-input>
+            <v-btn class="mr-4" @click="submit">Save</v-btn>
+        </form>
     </v-card>
 </template>
 <script>
-
+export default {
+    data() {
+        return {
+            file: null,
+        }
+    },
+    methods: {
+        submit(){
+            console.log(this.file);
+            let formData = new FormData();
+            formData.append('file', this.file);
+            this.$axios({
+                method: 'post',
+                url: this.$api_url + 'storeFile',
+                // headers: {},
+                data: formData
+            }).then(response => {
+                console.log(response);
+            }).catch((error) => {
+                console.log(error);
+            })
+        }
+    }
+}
 </script>
