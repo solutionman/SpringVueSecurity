c5e66b8fa4849f38596b9721e32881b87593415f
solutionman <solutionman@inbox.ru>
2021-09-09 11:14:08 +0700

    Files

--- frontvuebato/src/components/Files.vue
+++ frontvuebato/src/components/Files.vue
@@ -108,7 +108,7 @@
                     </v-dialog>
                     <v-dialog v-model="dialogDelete" max-width="500px">
                         <v-card>
-                            <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+                            <v-card-title class="text-h5">Are you sure you want to delete this file?</v-card-title>
                             <v-card-actions>
                                 <v-spacer></v-spacer>
                                 <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
